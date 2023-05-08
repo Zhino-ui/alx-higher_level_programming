362b2d38b421ae873b1a9a7e5b9bf3bc08a64c7c
Maxwell Ofori Asante <zhino4u2010@gmail.com>
2023-05-08 09:27:42 -0700

    6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 = str1 + " " + str2
-print("Welcome to {}!".format(str1))
+strr1 = "Holberton"
+strr2 = "School"
+strr = strr1 + " " + strr2
+print("Welcome to {}!".format(strr))
