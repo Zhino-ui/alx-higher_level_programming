03b1df2cfef6eaa31a499279f3aa0cf178e49b17
Maxwell Ofori Asante <zhino4u2010@gmail.com>
2023-05-08 14:08:52 -0700

    0x00-python-hello_world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * check_Cycle - checks cycle
+ * @list: linked list
+ * Return: value
+ */
+
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+	{
+		return (0)
+	}
+
+	while (1)
+	{
+		if (fast->next != NULL &&fast->next->next != NULL)
+		{
+			fast = fast->next->next;
+			slow = slow->next;
+
+			if (fast = slow)
+				return (1)
+		}
+		else
+			return (0);
+	}
+}
