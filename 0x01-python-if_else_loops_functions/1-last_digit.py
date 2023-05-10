3833d418a8cacaaa3e5fa3e8d9ea0b7bd76981a4
Maxwell Ofori Asante <zhino4u2010@gmail.com>
2023-05-10 02:57:48 -0700

    ./1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     dig = number % (-10)
-    print("Last digit of {} is {} abd is ".format(number, digit), end="")
+    print("Last digit of {} is {} abd is ".format(number, dig), end="")
 else:
     dig = number % 10
 if dig > 5:
