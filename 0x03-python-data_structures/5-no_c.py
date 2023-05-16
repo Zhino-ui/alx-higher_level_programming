179caf616e8f01c75dc8a3ec34cae3dd18391cf8
Maxwell Ofori Asante <zhino4u2010@gmail.com>
2023-05-16 07:09:46 -0700

    def no_c(my_string):

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = ""
-    for x in my_string:
-        if x.lower() != 'c':
-            new_string += x
-    return new_string
+    return ("".join(x for x in my_string if x not in "Xx"))
