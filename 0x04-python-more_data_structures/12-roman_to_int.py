10817d6fd14b89296be063189e59e3c98ead1f8b
Maxwell Ofori Asante <zhino4u2010@gmail.com>
2023-05-17 07:46:21 -0700

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
+
+    res = 0
+    prev_v = 0
+
+    for roman_char in reversed(roman_string):
+        value = roman_values.get(roman_char, 0)
+
+        if value >= prev_v:
+            res += value
+        else:
+            res -= value
+
+        prev_v = value
+    return res
